b09ad74a369284b3f54e51c54da3466fdac2f2d0
John Kenneth Romero <jeromero@csmbataan.ph.education>
2020-12-04 23:09:56 +0400

    added a non working code

---
+++ Country List (Foreach Loop Activity)/Countries.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Country_List__Foreach_Loop_Activity_
+{
+    public class Countries
+    {
+        public string countries { get; set; }
+        
+        public string countryname { get; set; }
+    }
+
+}
---
+++ Country List (Foreach Loop Activity)/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using RestSharp;
+using Newtonsoft;
+using Newtonsoft.Json;
+
+namespace Country_List__Foreach_Loop_Activity_
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var Client = new RestClient("http://techslides.com/demos/country-capitals.json");
+            var request = new RestRequest("", Method.GET);
+
+            IRestResponse response = Client.Execute(request);
+
+            var content = response.Content;
+
+            var data = JsonConvert.DeserializeObject<Countries>(content);
+            
+            
+        List<string> countries = new List<string>() { " ", " ", " ", " ", };
+        List<string> names = new List<string>();
+
+        int ctr = 0;
+            foreach (string country in countries)
+            {
+                names.Add(ctr + "" + country);
+                ctr = ctr + 1;
+         }
+            lstCountries.ItemsSource = names;
+        }
+    }
+}
